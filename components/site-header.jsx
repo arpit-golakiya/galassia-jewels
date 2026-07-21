5cd4c0a501f7dc7b1ef8f0769a668ff8a7e8f431
arpit-golakiya <arpitgolakiya97@gmail.com>
2026-07-21 14:49:27 +0530

    fix: change logo of galassia jewels.

--- components/site-header.jsx
+++ components/site-header.jsx
@@ -6,7 +6,7 @@ export function SiteHeader() {
       <div className="mx-auto flex w-full items-center justify-center px-2 py-2 sm:px-6 md:py-6 lg:max-w-[1280px]">
         <a href="/" aria-label="Galassia Jewels" className="block w-full sm:w-auto">
           <img
-            src="/logo.png"
+            src="/new_logo.png"
             alt="Galassia Jewels"
             className="mx-auto h-auto w-full max-h-[140px] object-contain sm:h-20 sm:w-auto sm:max-h-none"
             onError={(e) => {
